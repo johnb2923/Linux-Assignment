f80e9781580686a85c1430a5f5720ee9c0ef0ce0
johnb2923 <jbolton2923@gmail.com>
2025-06-03 22:12:18 -0400

    Update init_lab.sh
    eng add

--- init_lab.sh
+++ init_lab.sh
@@ -3,8 +3,12 @@
 apt update && apt upgrade -y
 apt install -y curl wget tree htop git net-tools
 
+if ! getent group engineering > /dev/null; then
+    groupadd engineering
+fi
+
 for user in "${USERS[@]}"; do
-    useradd -m "$user"
+    useradd -m -g engineering "$user"
     echo "$user:$DEFAULT_PASSWORD" | chpasswd
     chage -d 0 "$user"
 done
