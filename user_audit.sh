c32708f7d4965c04fd67499880add0a169044204
johnb2923 <jbolton2923@gmail.com>
2025-06-04 11:14:46 -0400

    Update user_audit.sh

--- user_audit.sh
+++ user_audit.sh
@@ -7,3 +7,9 @@ echo -e "\n-- Users with sudo access --"
 echo "Users with sudo group:"
 getent group sudo | cut -d: -f4 | sed 's/,/\n/g'
 
+echo -e "\n-- Users logged in within the last 24 hours --"
+if command -v last &> /dev/null; then
+    last -s "$(date -d '24 hours ago' +%Y-%m-%d %H:%M:%S)" | awk '{print $1}' | sort -u | grep -v 'wtmp' | grep -v 'reboot' | grep -v '^$'
+else
+    echo "No users logged in."
+fi
