eda034a1832971346f995085bf0330a15f4d7045
johnb2923 <jbolton2923@gmail.com>
2025-06-03 22:09:42 -0400

    Update init_lab.sh
    add users

--- init_lab.sh
+++ init_lab.sh
@@ -2,3 +2,9 @@
 
 apt update && apt upgrade -y
 apt install -y curl wget tree htop git net-tools
+
+for user in "${USERS[@]}"; do
+    useradd -m "$user"
+    echo "$user:$DEFAULT_PASSWORD" | chpasswd
+    chage -d 0 "$user"
+done
