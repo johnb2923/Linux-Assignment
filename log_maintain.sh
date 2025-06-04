07dd65adfdfb36c70aff933161443c6582ccc253
johnb2923 <jbolton2923@gmail.com>
2025-06-03 22:40:41 -0400

    Update log_maintain.sh

--- log_maintain.sh
+++ log_maintain.sh
@@ -2,3 +2,10 @@
 
 mkdir -p "/opt/logmanager"
 mkdir -p "/opt/archive_logs"
+
+touch -d "2 days ago" "/opt/logmanager/app_log_$(date -d '2 days ago' +%Y%m%d%H%M%S).log"
+touch -d "10 days ago" "/opt/logmanager/system_log_$(date -d '10 days ago' +%Y%m%d%H%M%S).log"
+touch -d "20 days ago" "/opt/logmanager/web_log_$(date -d '20 days ago' +%Y%m%d%H%M%S).log"
+touch -d "35 days ago" "/opt/logmanager/db_log_$(date -d '35 days ago' +%Y%m%d%H%M%S).log"
+touch -d "40 days ago" "/opt/logmanager/security_log_$(date -d '40 days ago' +%Y%m%d%H%M%S).log"
+
