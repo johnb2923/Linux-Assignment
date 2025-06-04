008b214d609f5fc714d9ffe1214da15df31f09db
johnb2923 <jbolton2923@gmail.com>
2025-06-04 11:10:29 -0400

    Update user_audit.sh

--- user_audit.sh
+++ user_audit.sh
@@ -6,3 +6,10 @@ awk -F':' '($3 >= 1000) {print $1}' /etc/passwd
 echo -e "\n-- Users with sudo access --"
 echo "Users with sudo group:"
 getent group sudo | cut -d: -f4 | sed 's/,/\n/g'
+
+echo "Users with direct sudo:"
+grep -E '^%sudo|^root|^[a-zA-Z0-9_]+ ALL=' /etc/sudoers /etc/sudoers.d/* 2>/dev/null | \
+grep -v '^#' | \
+awk '{print $1}' | \
+sed 's/%//' | \
+sort -u
