96636d16b416c8cc0df7e6e13674582c2549e7b8
johnb2923 <jbolton2923@gmail.com>
2025-06-04 10:59:51 -0400

    Update user_audit.sh
    sudo group listing

--- user_audit.sh
+++ user_audit.sh
@@ -2,3 +2,7 @@
 
 echo "-- Users with UID >= 1000 --"
 awk -F':' '($3 >= 1000) {print $1}' /etc/passwd
+
+echo -e "\n-- Users with sudo access --"
+echo "Users with sudo group:"
+getent group sudo | cut -d: -f4 | sed 's/,/\n/g'
