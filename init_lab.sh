04d44f08ea232bd33962f92232d3829ce2669653
johnb2923 <jbolton2923@gmail.com>
2025-06-03 22:13:03 -0400

    Update init_lab.sh

--- init_lab.sh
+++ init_lab.sh
@@ -7,6 +7,9 @@ if ! getent group engineering > /dev/null; then
     groupadd engineering
 fi
 
+USERS=("devops" "qa" "intern")
+DEFAULT_PASSWORD="password123"
+
 for user in "${USERS[@]}"; do
     useradd -m -g engineering "$user"
     echo "$user:$DEFAULT_PASSWORD" | chpasswd
